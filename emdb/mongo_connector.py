ea225e46b7bbd400e26ee28ea3bc7ccead5ad985
techbot123 <sagarck44@gmail.com>
2021-12-27 22:18:15 -0800

    make it better 2

--- emdb/mongo_connector.py
+++ emdb/mongo_connector.py
@@ -5,7 +5,7 @@ from getpass import getpass
 
 class MongoCon:
 	def get_mongo_client(self, username, pwd):
-		self.client = MongoClient('mongodb+srv://{}:{}@cluster0.n1b6n.mongodb.net/Employee?retryWrites=true&w=majority'.format(username, urllib.parse.quote_plus(pwd)))
+		self.client = MongoClient('mongodb+srv://{}:{}@cluster0.n1b6n.mongodb.net/Employee?retryWrites=true&w=majority'.format(username, pwd))
 		try:
 			self.client.list_database_names()
 		except Exception as e:
@@ -14,9 +14,6 @@ class MongoCon:
 			return self.client
 
 	def mongo_connector(self):
-		# username = input('Enter Username for Mongo DB: ')
 		username = 'techsy'
-		# pwd = getpass(prompt = 'Enter Password: ')
 		pwd = 'hehe'
-		# print(username, pwd)
 		return self.get_mongo_client(username, pwd)
