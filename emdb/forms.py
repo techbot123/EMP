b5949f73b775b0d16036b0fbc5818b1ebbae8609
techbot123 <sagarck44@gmail.com>
2021-12-27 22:51:27 -0800

    make it better 3

--- emdb/forms.py
+++ emdb/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from wtforms.fields.html5 import DateField,DateTimeField
+from wtforms.fields import DateField,DateTimeField
 from flask_wtf.file import FileField
 import phonenumbers
 from emdb import db
